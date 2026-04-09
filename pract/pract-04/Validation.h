fe03a3cee6516e677e60a55b721b79dfeda5671d
ivaparunova-sudo <iva.parunova@gmail.com>
2026-04-09 15:17:24 +0300

    Revert "ivaparunova_pract4_ex1"

--- pract/pract-04/Validation.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Language.h"
-
-namespace Validation {
-    bool isValidName(const char* name);
-    bool isValidAge(unsigned age);
-    bool isValidSalary(unsigned salary);
-    bool isValidLang(Language lang);
-}
